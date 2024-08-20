159d67fe550d12fcb396607ea9353a3d976facbd
atul <atulgupta.d796@gmil.com>
2024-08-20 13:50:57 +0530

    20-8-24 v2

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProjectCard({ url, name, descrption, image }) {
   return (
     <a href={url} target="_blank">
-      <img className="hover" src={image} alt={`${name} logo`} />
+      <img src={image} alt={`${name} logo`} />
       <h3>{name}</h3>
       <p>{descrption}</p>
     </a>
--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./ContactStyles.module.css";
 
 function Contact() {
+  //   useEffect(() => {}, []);
+  window.onload = function () {
+    document.querySelector('input[type="email"]').style.backgroundImage =
+      "none";
+  };
+
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
+      <form action="">
+        <div className="formGroup">
+          <label htmlFor="name" hidden>
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="email" hidden>
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+            style={{ background: "none" }}
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="message" hidden>
+            Message
+          </label>
+          <textarea
+            type="text"
+            name="message"
+            id="message"
+            placeholder="Message"
+            required
+          />
+        </div>
+        <input type="submit" className="hover btn" value="Submit" />
+      </form>
     </section>
   );
 }
